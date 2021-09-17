24b2fb8f70ae9138282f6ce6a79a98940d8da995
Haziq Hapiz <zyqq.dev@gmail.com>
2021-09-17 20:14:56 +0800

    init

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Semakan</title>
+    
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div>
+        <div class="bg-gray-500">
+            <h1>
+                TEST
+            </h1>
+            @yield('content')
+        </div>
+    </div>
+</body>
+</html>
