715fe1b7e4e5e70c5e5665f917b4b3bdf99ec255
Haziq Hapiz <zyqq.dev@gmail.com>
2021-09-22 21:26:37 +0800

    add semakan checker

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Senarai;
+use App\Models\Tarikh;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -28,22 +30,42 @@ class CheckController extends Controller
             $stats = User::where('phone', $request->input('phone'))->first()->status_vaksin;
             $nama = User::where('phone', $request->input('phone'))->first()->name;
 
-            // dd($nama);
-
-            // dd($stats);
             
-            if($stats == 2) {
-                // $nama = DB::select('select name from users where phone = ?', [$request->input('phone')]);
-                return redirect('/keputusan')->with('vaksin', 'Fully Vaccinated')->with('nama', $nama);
+            if (Senarai::where('phone_id', $request->input('phone'))->count() > 0) {
+                
+                $keputusan = Senarai::where('phone_id', $request->input('phone'))->first()->status_pendaftaran;
+                // $keputusan = DB::table('senarai_nama')->select('status_pendaftaran')->where('phone_id', $request->input('phone'))->get();
+                // select status_pendaftaran from senarai_nama where phone_id = '$request->input('phone')'
+                // $keputusan = DB::table('senarai_nama')->select('status_pendaftaran')->where('phone_id', $request->input('phone'))->first();
+
+                // $keputusan = DB::select('select status_pendaftaran from senarai_nama where phone_id = ?', [$request->input('phone')]->first());
+
+                // dd($keputusan);
+
+                // dd($stats);
+
+                if ($keputusan == 2) {
+                    // return back()->with('status', 'Pendaftaran telah ditutup.');
+                    return redirect('/keputusan')->with('berjaya', 'Pendaftaran berjaya')->with('nama', $nama)->with('vaksin', 'Fully Vaccinated');
+                }
+                else if ($keputusan == 1) {
+                    return redirect('/keputusan')->with('gagal', 'Pendaftaran tidak berjaya')->with('nama', $nama)->with('vaksin', 'Fully Vaccinated');
+                }
+            
+            // THISS IS FOR Dummy 
+            // if($stats == 2) {
+            //     // $nama = DB::select('select name from users where phone = ?', [$request->input('phone')]);
+            //     return redirect('/keputusan')->with('vaksin', 'Fully Vaccinated')->with('nama', $nama);
+            // }
+            // else {
+            //     return back()->with('vaksin', 'Partially Vaccinated');
+            // }
             }
+            
             else {
-                return back()->with('vaksin', 'Partially Vaccinated');
+                return redirect('/semak')->with('status', 'Nombor ini tidak berdaftar lagi. Sila buat pendaftaran giliran.');
             }
         }
-
-        // if ($request->only('phone')) {
-        //     return back()->with('status', 'Nombor ini tiada dalam sistem. Sila buat pendaftaran baru');
-        // }
 
         // return redirect('/');
     }
--- resources/views/layouts/semak/keputusan.blade.php
+++ resources/views/layouts/semak/keputusan.blade.php
@@ -7,11 +7,12 @@
             <br>
             MASJID KAMPUNG JOHAN SETIA KLANG
         </h1>
-        @if(session()->has('vaksin'))
+        @if(session()->has('berjaya'))
             <div class="bg-green-500 py-4 px-2 w-10/12 rounded-lg mt-2 text-white font-semibold text-center justify-center mx-auto">
                 {{ session()->get('vaksin') }}
                 <h3>
-                    Berjaya!
+                    {{-- Berjaya! --}}
+                    {{ session()->get('berjaya') }}
                 </h3>
                 {{ session()->get('nama') }}
                 <h4>
@@ -21,6 +22,17 @@
                     Kegagalan anda untuk hadir akan menyebabkan nama anda disenarai hitam.
                 </h5>
             </div>
+            @elseif(session()->has('gagal'))
+            <div class="bg-red-500 py-4 px-2 w-10/12 rounded-lg mt-2 text-white font-semibold text-center justify-center mx-auto">
+                {{ session()->get('vaksin') }}
+                <h3>
+                    {{-- Gagal! --}}
+                    {{ session()->get('gagal') }}
+                </h3>
+                {{ session()->get('nama') }}
+                <h4>
+                    Nama anda akan dimasukkan dalam senarai putih untuk giliran minggu hadapan.
+                </h4>
         @endif
     </div>
 @endsection
